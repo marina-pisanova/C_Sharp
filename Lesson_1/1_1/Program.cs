f933fd0a6ed4a904fc0513e0d1fca19b18a93657
Marina P <marina.pisanova@gmail.com>
2023-12-25 20:49:00 +0300

    Class work_task 1.1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 25;
+int b = 5;
+
+if (b * b == a)
+    Console.WriteLine("true");
+else
+    Console.WriteLine("false");
+
+
