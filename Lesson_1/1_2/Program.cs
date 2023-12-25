77e852925528f7adfd5289633711057d952fa14e
Marina P <marina.pisanova@gmail.com>
2023-12-25 21:28:53 +0300

    Class work_task 1.2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 5;
+int i = -a;
+while(i <= a)
+{
+    Console.Write(i + " ");
+    i++;
+}
