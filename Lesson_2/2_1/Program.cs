c5d49f51d684c8d518c4d39c61570ce834d384ec
Marina P <marina.pisanova@gmail.com>
2024-01-04 16:08:57 +0300

    Задача 1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// 1. Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого числа.
+Console.WriteLine("Enter number");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a / 100 * 10 + a % 10);
