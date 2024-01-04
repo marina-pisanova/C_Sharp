220c4f39399e3f80105f872901d9778be3b5da24
Marina P <marina.pisanova@gmail.com>
2024-01-04 19:03:01 +0300

    Task 2_4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая выводит третью с конца 
+// цифру заданного числа или сообщает, что третьей цифры нет.
+
+int a = int.Parse(Console.ReadLine()!);
+if(a<100){Console.WriteLine("no");}
+else{Console.WriteLine("yes" +a/100%10);}
