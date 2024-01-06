9387be55dd1d0fb3f1022368265ebf0d44f7f63b
Marina P <marina.pisanova@gmail.com>
2024-01-06 17:38:17 +0300

    Task2. Номер координатной четверти

---
+++ HomeWork/HW2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
+//в которой находится эта точка.
+
+int x = int.Parse(Console.ReadLine()!);
+int y = int.Parse(Console.ReadLine()!);
+
+if(x > 0 && y > 0){Console.WriteLine("1");}
+    if(x < 0 && y > 0){Console.WriteLine("2");}
+        if(x < 0 && y < 0){Console.WriteLine("3");}
+else{Console.WriteLine("4");}
