9ee6e76b394657420ebc78a2e26475b828244d10
Marina P <marina.pisanova@gmail.com>
2024-01-08 23:38:23 +0300

    заание 3

---
+++ HomeWork/HW3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+Console.Write("Enter number [10, 99]: ");
+int a = int.Parse(Console.ReadLine()!);
+int FirstNum = a / 10;
+int SecondNum = a % 10;
+int max = FirstNum  > SecondNum ? FirstNum  : SecondNum;
+Console.WriteLine(max);
