1fccd614e4499b3ac59a2a291588229d45f29be0
Marina P <marina.pisanova@gmail.com>
2024-01-11 21:35:21 +0300

    Task2

---
+++ Lesson_Mas_1/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создать массив на 10 элементов, Заполнить его целыми числами, Вывести элементы на экран
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+// while (i < n)
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // coconsole.Write(arr[i]);
+    // console.Write(arr[' ']);
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+    
+}
