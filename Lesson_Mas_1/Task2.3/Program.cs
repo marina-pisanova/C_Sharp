66a6eecd8e276d9b6e6f687b086cb6dac38f5252
Marina P <marina.pisanova@gmail.com>
2024-01-11 22:26:58 +0300

    Task2.3

---
+++ Lesson_Mas_1/Task2.3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано 5 гирь с разным весом в случайном порядке. Определить вес самой тяжелой гири
+
+int n = 5;
+int[] arr = {2,6,3,9,1};
+int i = 0;
+int max = arr[0];
+
+while(i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
