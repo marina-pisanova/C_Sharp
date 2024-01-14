214c0fb1690a6d57c68b39e1f6d443c6c90fec3c
Marina P <marina.pisanova@gmail.com>
2024-01-14 15:11:58 +0300

    while, each, foreach

--- Lesson_Mas_1/Task2.4/Program.cs
+++ Lesson_Mas_1/Task2.4/Program.cs
@@ -1,8 +1,29 @@
-﻿// 5 задача с 5 гирями, найти максимальную, с FOREACH
+﻿// 5 задача с 5 гирями, найти максимальную, с While, FOR,  FOREACH
 int n = 5;
 int[] arr = { 3, 6, 1, 8, 2 };
 int i = 0;
 int max = arr[0];
+
+while(i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = arr[0];
+for (int j = 0; j < n; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+Console.WriteLine(max);
+
 
 max = arr[0];
 foreach (int e in arr)
