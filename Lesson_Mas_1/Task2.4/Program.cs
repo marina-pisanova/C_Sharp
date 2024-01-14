410c17f8f9473c7c0b5cb43b2e7738a1eb9182c7
Marina P <marina.pisanova@gmail.com>
2024-01-14 15:01:31 +0300

    Task2.4

---
+++ Lesson_Mas_1/Task2.4/Program.cs
@@ -0,0 +1,15 @@
+﻿// 5 задача с 5 гирями, найти максимальную, с FOREACH
+int n = 5;
+int[] arr = { 3, 6, 1, 8, 2 };
+int i = 0;
+int max = arr[0];
+
+max = arr[0];
+foreach (int e in arr)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
